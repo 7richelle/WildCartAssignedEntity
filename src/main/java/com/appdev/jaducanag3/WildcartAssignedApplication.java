3dc83b2d254e03a12b6ce04450eabeed0dd1eb37
7richelle <162084087+7richelle@users.noreply.github.com>
2025-10-22 12:06:30 +0800

    wildcart assigned

---
+++ src/main/java/com/appdev/jaducanag3/WildcartAssignedApplication.java
@@ -0,0 +1,13 @@
+package com.appdev.jaducanag3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WildcartAssignedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WildcartAssignedApplication.class, args);
+	}
+
+}
